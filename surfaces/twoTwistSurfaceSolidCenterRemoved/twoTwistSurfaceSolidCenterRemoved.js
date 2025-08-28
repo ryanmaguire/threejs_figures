72632dbc077a084e8f38395466f00c90d3bd99a6
ryanmaguire <rmaguire@protonmail.com>
2025-08-28 16:48:34 -0400

    Updates.

--- surfaces/twoTwistSurfaceSolidCenterRemoved/twoTwistSurfaceSolidCenterRemoved.js
+++ surfaces/twoTwistSurfaceSolidCenterRemoved/twoTwistSurfaceSolidCenterRemoved.js
@@ -15,10 +15,10 @@
  *  along with this file.  If not, see <https://www.gnu.org/licenses/>.       *
  ******************************************************************************
  *  Purpose:                                                                  *
- *      Renders a wireframe Mobius strip.                                     *
+ *      Renders a two twist surface with the center removed.                  *
  ******************************************************************************
  *  Author:     Ryan Maguire                                                  *
- *  Date:       July 27, 2025                                                 *
+ *  Date:       August 27, 2025                                               *
  ******************************************************************************/
 
 /*  three.js has all of the tools for generating 3D animations.               */
