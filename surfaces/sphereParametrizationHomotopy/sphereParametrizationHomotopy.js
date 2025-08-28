a6e60cf7b1ea86763c4cf3e161fd0ce733692471
ryanmaguire <rmaguire@protonmail.com>
2025-08-28 16:43:42 -0400

    Fixing more dates.

--- surfaces/sphereParametrizationHomotopy/sphereParametrizationHomotopy.js
+++ surfaces/sphereParametrizationHomotopy/sphereParametrizationHomotopy.js
@@ -18,7 +18,7 @@
  *      Renders a parametrization from a plane to the sphere.                 *
  ******************************************************************************
  *  Author:     Ryan Maguire                                                  *
- *  Date:       June 21, 2025                                                 *
+ *  Date:       July 3, 2025                                                  *
  ******************************************************************************/
 
 /*  three.js has all of the tools for generating 3D animations.               */
