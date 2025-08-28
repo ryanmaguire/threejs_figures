a5738532965707fd87377cfcd830c1914c88cb80
ryanmaguire <rmaguire@protonmail.com>
2025-08-28 16:49:15 -0400

    Updates.

--- surfaces/twoTwistSurfaceSolidHomotopy/twoTwistSurfaceSolidHomotopy.js
+++ surfaces/twoTwistSurfaceSolidHomotopy/twoTwistSurfaceSolidHomotopy.js
@@ -15,10 +15,10 @@
  *  along with this file.  If not, see <https://www.gnu.org/licenses/>.       *
  ******************************************************************************
  *  Purpose:                                                                  *
- *      Renders a homotopy from a plane to a Mobius strip.                    *
+ *      Renders a homotopy from a plane to a two twist surface.               *
  ******************************************************************************
  *  Author:     Ryan Maguire                                                  *
- *  Date:       July 18, 2025                                                 *
+ *  Date:       August 27, 2025                                               *
  ******************************************************************************/
 
 /*  three.js has all of the tools for generating 3D animations.               */
