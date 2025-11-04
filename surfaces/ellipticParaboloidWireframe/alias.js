efa3b871b9d2a36b54731bb7a0476e443d742e33
ryanmaguire <rmaguire@protonmail.com>
2025-11-04 14:02:45 -0500

    Moving alias file to the C source directory, it is only used by emscripten.

--- surfaces/ellipticParaboloidWireframe/alias.js
+++
@@ -1,6 +0,0 @@
-createModule.generateIndices = createModule._generate_indices;
-createModule.generateMesh = createModule._generate_mesh;
-createModule.getIndexBuffer = createModule._get_index_buffer;
-createModule.getMeshBuffer = createModule._get_mesh_buffer;
-createModule.rotateMesh = createModule._rotate_mesh;
-createModule.setRotationAngle = createModule._set_rotation_angle;
