4089bdb60cd8ff10188aa01263d8a0584f2064c1
ryanmaguire <rmaguire@protonmail.com>
2025-11-04 13:06:12 -0500

    Giving up on rust.

--- surfaces/ellipticParaboloidWireframe/rustsrc/get_index_buffer.rs
+++ surfaces/ellipticParaboloidWireframe/rustsrc/get_index_buffer.rs
@@ -1,6 +1,7 @@
 use std::sync::Mutex;
 use crate::{INDEX_BUFFER, INDEX_SIZE};
 
-pub fn get_index_buffer() -> &'static Mutex<[u32; INDEX_SIZE]> {
-    &INDEX_BUFFER
+pub fn get_index_buffer() -> usize {
+    let ptr: *const Mutex<[u32; INDEX_SIZE]> = &INDEX_BUFFER;
+    return ptr as usize;
 }
--- surfaces/ellipticParaboloidWireframe/rustsrc/get_mesh_buffer.rs
+++ surfaces/ellipticParaboloidWireframe/rustsrc/get_mesh_buffer.rs
@@ -1,6 +1,7 @@
 use std::sync::Mutex;
 use crate::{MESH_BUFFER, MESH_SIZE};
 
-pub fn get_mesh_buffer() -> &'static Mutex<[f32; MESH_SIZE]> {
-    &MESH_BUFFER
+pub fn get_mesh_buffer() -> usize {
+    let ptr: *const Mutex<[f32; MESH_SIZE]> = &MESH_BUFFER;
+    return ptr as usize;
 }
