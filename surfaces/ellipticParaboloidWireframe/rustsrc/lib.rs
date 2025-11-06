33f3526581532e4133dc16bf0474f77ebf31e32b
ryanmaguire <rmaguire@protonmail.com>
2025-11-06 17:16:08 -0500

    Getting the rust version to (finally) work in a browser.

--- surfaces/ellipticParaboloidWireframe/rustsrc/lib.rs
+++ surfaces/ellipticParaboloidWireframe/rustsrc/lib.rs
@@ -24,12 +24,13 @@
 #![crate_name = "paraboloid"]
 
 use std::sync::Mutex;
+use wasm_bindgen::prelude::*;
 
 pub const MAX_WIDTH: u32 = 512;
 pub const MAX_HEIGHT: u32 = 512;
 
 pub const MAX_LENGTH: u32 = MAX_HEIGHT * MAX_WIDTH;
-pub const MESH_SIZE: usize = (32 * MAX_LENGTH) as usize;
+pub const MESH_SIZE: usize = (3 * MAX_LENGTH) as usize;
 pub const INDEX_SIZE: usize = (2*(2*MAX_LENGTH-MAX_WIDTH-MAX_HEIGHT)) as usize;
 
 pub static ROTATION_ANGLE: Mutex<f32> = Mutex::new(0.0);
@@ -51,3 +52,33 @@ pub mod get_index_buffer;
 pub mod get_mesh_buffer;
 pub mod rotate_mesh;
 pub mod set_rotation_angle;
+
+#[wasm_bindgen(js_name = "generateIndices")]
+pub fn wasm_generate_indices(ptr: *mut u32, nx_pts: u32, ny_pts: u32) {
+    generate_indices::generate_indices(ptr, nx_pts, ny_pts);
+}
+
+#[wasm_bindgen(js_name = "generateMesh")]
+pub fn wasm_generate_mesh(ptr: *mut f32, nx_pts: u32, ny_pts: u32) {
+    generate_mesh::generate_mesh(ptr, nx_pts, ny_pts);
+}
+
+#[wasm_bindgen(js_name = "getIndexBuffer")]
+pub fn wasm_get_index_buffer() -> usize {
+    return get_index_buffer::get_index_buffer();
+}
+
+#[wasm_bindgen(js_name = "getMeshBuffer")]
+pub fn wasm_get_mesh_buffer() -> usize {
+    return get_mesh_buffer::get_mesh_buffer();
+}
+
+#[wasm_bindgen(js_name = "rotateMesh")]
+pub fn wasm_rotate_mesh(ptr: *mut f32, n_pts: u32) {
+    rotate_mesh::rotate_mesh(ptr, n_pts);
+}
+
+#[wasm_bindgen(js_name = "setRotationAngle")]
+pub fn wasm_set_rotation_angle(angle: f32) {
+    set_rotation_angle::set_rotation_angle(angle);
+}
