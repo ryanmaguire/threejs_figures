ed37758ba6196293086966551971a4eeb6cc4815
ryanmaguire <rmaguire@protonmail.com>
2025-11-19 10:55:50 -0500

    Updating to use Google's style guide, fixed some typos, added comments.

--- surfaces/ellipticParaboloidWireframe/gosrc/generate_mesh.go
+++ surfaces/ellipticParaboloidWireframe/gosrc/generate_mesh.go
@@ -23,23 +23,25 @@
 package main
 
 /*  Function for generating the mesh for the surface by calculating vertices. */
-func generateMesh(arr []float32, nx_pts, ny_pts uint32) {
+func generateMesh(arr []float32, nxPts, nyPts uint32) {
 
     /*  Step sizes in the horizontal and vertical axes.                       */
-    var dx float32 = paraboloid_width / float32(nx_pts - 1);
-    var dy float32 = paraboloid_height / float32(ny_pts - 1);
-    const height_shift float32 = -2.0;
+    var dx float32 = width / float32(nxPts - 1)
+    var dy float32 = height / float32(nyPts - 1)
+
+    /*  Shift for centering the object around the origin.                     */
+    const heightShift float32 = -2.0
 
     /*  Variables for indexing the horizontal and vertical axes.              */
-    var x_index, y_index uint32;
+    var xIndex, yIndex uint32
 
     /*  Variable for indexing over the array being written to.                */
-    var index uint32 = 0;
+    var index uint32 = 0
 
-    /*  Avoiding writing beyond the bounds of the array that was allocated.   *
+    /*  Avoid writing beyond the bounds of the array that was allocated.      *
      *  Check if the input sizes are too big.                                 */
-    if (nx_pts > MAX_WIDTH) || (ny_pts > MAX_HEIGHT) {
-        return;
+    if (nxPts > maxWidth) || (nyPts > maxHeight) {
+        return
     }
 
     /*  Loop through the vertical axis. The elliptic paraboloid lies          *
@@ -47,31 +49,31 @@ func generateMesh(arr []float32, nx_pts, ny_pts uint32) {
      *                                                                        *
      *  Note, since the y index is the outer for-loop, the array is indexed   *
      *  in row-major fashion. That is, index = y * width + x.                 */
-    for y_index = 0; y_index < ny_pts; y_index++ {
+    for yIndex = 0; yIndex < nyPts; yIndex++ {
 
         /*  Convert pixel index to y coordinate.                              */
-        var y_pt float32 = paraboloid_y_start + float32(y_index) * dy
+        var yPt float32 = yStart + float32(yIndex) * dy
 
         /*  Loop through the horizontal component of the object.              */
-        for x_index = 0; x_index < nx_pts; x_index++ {
+        for xIndex = 0; xIndex < nxPts; xIndex++ {
 
             /*  Convert pixel index to x coordinate in the plane.             */
-            var x_pt float32 = paraboloid_x_start + float32(x_index) * dx;
+            var xPt float32 = xStart + float32(xIndex) * dx
 
             /*  The elliptic paraboloid has a simple formula: z = x^2 + 2y^2. *
              *  We shift this slightly to center the surface on the screen.   */
-            var z_pt float32 = x_pt * x_pt + 2.0 * y_pt * y_pt + height_shift;
+            var zPt float32 = xPt * xPt + 2.0 * yPt * yPt + heightShift
 
             /*  Add this point to our vertex array.                           */
-            arr[index] = x_pt;
-            arr[index + 1] = y_pt;
-            arr[index + 2] = z_pt;
+            arr[index] = xPt
+            arr[index + 1] = yPt
+            arr[index + 2] = zPt
 
             /*  Move on to the next point in the mesh. A point needs 3 floats.*/
-            index += 3;
+            index += 3
         }
         /*  End of horizontal for-loop.                                       */
     }
     /*  End of vertical for-loop.                                             */
 }
-/*  End of generate_mesh.                                                     */
+/*  End of generateMesh.                                                      */
