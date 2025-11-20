dd7801948bcb592ff56f93c5194a04b37d25c14f
ryanmaguire <rmaguire@protonmail.com>
2025-11-20 14:35:49 -0500

    Some more functions for three.js figures.

---
+++ common/gosrc/threetools/reset_index_buffer.go
@@ -0,0 +1,41 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is free software: you can redistribute it and/or modify         *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  This file is distributed in the hope that it will be useful,              *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with this file.  If not, see <https://www.gnu.org/licenses/>.       *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Resets the size of the index buffer inside a canvas.                  *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       November 20, 2025                                             *
+ ******************************************************************************/
+package threetools
+
+/******************************************************************************
+ *  Function:                                                                 *
+ *      ResetIndexBuffer                                                      *
+ *  Purpose:                                                                  *
+ *      Resets the size of the index buffer.                                  *
+ *  Arguments:                                                                *
+ *      canvas (*Canvas):                                                     *
+ *          The canvas that is being resized.                                 *
+ *      buffer ([]uint32]):                                                   *
+ *          The buffer where canvas will store its data.                      *
+ *  Output:                                                                   *
+ *      None.                                                                 *
+ ******************************************************************************/
+func ResetIndexBuffer(canvas *Canvas, buffer []uint32) {
+    SetIndexSize(canvas)
+    canvas.Indices = buffer[0:canvas.IndexSize]
+}
---
+++ common/gosrc/threetools/reset_mesh_buffer.go
@@ -0,0 +1,42 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is free software: you can redistribute it and/or modify         *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  This file is distributed in the hope that it will be useful,              *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with this file.  If not, see <https://www.gnu.org/licenses/>.       *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Resets the size of the mesh buffer inside a canvas.                   *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       November 20, 2025                                             *
+ ******************************************************************************/
+package threetools
+
+/******************************************************************************
+ *  Function:                                                                 *
+ *      ResetMeshBuffer                                                       *
+ *  Purpose:                                                                  *
+ *      Resets the size of the mesh buffer.                                   *
+ *  Arguments:                                                                *
+ *      canvas (*Canvas):                                                     *
+ *          The canvas that is being resized.                                 *
+ *      buffer ([]float32]):                                                  *
+ *          The buffer where canvas will store its data.                      *
+ *  Output:                                                                   *
+ *      None.                                                                 *
+ ******************************************************************************/
+func ResetMeshBuffer(canvas *Canvas, buffer []float32) {
+    canvas.NumberOfPoints = int(canvas.NxPts * canvas.NyPts)
+    canvas.MeshSize = 3 * canvas.NumberOfPoints
+    canvas.Mesh = buffer[0:canvas.MeshSize]
+}
