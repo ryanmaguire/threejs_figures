49a6cd3313278a36f7224d55cb8e69a316a2a9dc
ryanmaguire <rmaguire@protonmail.com>
2025-11-21 12:58:37 -0500

    Adding license.

--- common/jssrc/index.js
+++ common/jssrc/index.js
@@ -1,3 +1,27 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of threejs_figures.                                     *
+ *                                                                            *
+ *  threejs_figures is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  threejs_figures is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with threejs_figures.  If not, see <https://www.gnu.org/licenses/>. *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Exports all of the functions from common/jssrc/.                      *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       November 21, 2025                                             *
+ ******************************************************************************/
 export {basicWireframe} from "./basicWireframe.js";
 export {sceneCamera} from "./sceneCamera.js";
 export {sceneFromSurface} from "./sceneFromSurface.js";
