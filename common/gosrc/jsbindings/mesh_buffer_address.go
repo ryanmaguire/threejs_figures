6f977d7cc76c74721db8e33a2dc7ff2f8ad54d49
ryanmaguire <rmaguire@protonmail.com>
2025-11-22 10:45:46 -0500

    Organizing go src.

--- common/gosrc/jsbindings/mesh_buffer_address.go
+++ common/gosrc/jsbindings/mesh_buffer_address.go
@@ -15,21 +15,22 @@
  *  along with this file.  If not, see <https://www.gnu.org/licenses/>.       *
  ******************************************************************************
  *  Purpose:                                                                  *
- *      Exports Go functions to JavaScript.                                   *
+ *      Provides a JS binding for MeshBufferAddress.                          *
  ******************************************************************************
  *  Author:     Ryan Maguire                                                  *
  *  Date:       November 19, 2025                                             *
  ******************************************************************************/
 package jsbindings
 
-import "syscall/js"
+import (
+    "syscall/js"
+    "common/threetools"
+)
 
-func ExportGoFunctions() {
+/*  Wrapper function for the Go function MeshBufferAddress.                   */
+func MeshBufferAddress(this js.Value, args []js.Value) interface{} {
 
-    /*  Create JavaScript wrappers the function, using standard camel case.   */
-    var window js.Value = js.Global()
-    window.Set("rotateMesh", js.FuncOf(RotateMesh))
-    window.Set("setRotationAngle", js.FuncOf(SetRotationAngle))
-    window.Set("meshBufferAddress", js.FuncOf(MeshBufferAddress))
-    window.Set("indexBufferAddress", js.FuncOf(IndexBufferAddress))
+    /*  No input, this function simply retrieves the address of messBuffer.   */
+    return threetools.MeshBufferAddress()
 }
+/*  End of MeshBufferAddress.                                                 */
