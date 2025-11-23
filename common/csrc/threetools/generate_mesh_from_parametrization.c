a1f2637de539decf3758e072fa2dbdef13c418a4
ryanmaguire <rmaguire@protonmail.com>
2025-11-23 12:28:40 -0500

    Moving C functions to the common directory.

---
+++ common/csrc/threetools/generate_mesh_from_parametrization.c
@@ -0,0 +1,95 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of threejs_figures.                                     *
+ *                                                                            *
+ *  threejs_figures is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  threejs_figures is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with threejs_figures.  If not, see <https://www.gnu.org/licenses/>. *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Computes the locations of the points in the mesh for a surface.       *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       October 30, 2025                                              *
+ ******************************************************************************/
+
+/*  Canvas and SurfaceParametrization typedefs found here.                    */
+#include <threetools/types.h>
+
+/*  The MAX_WIDTH and MAX_HEIGHT macros are defined here.                     */
+#include <threetools/globals.h>
+
+/*  Function prototype / forward declaration given here.                      */
+#include <threetools/threetools.h>
+
+/******************************************************************************
+ *  Function:                                                                 *
+ *      generate_mesh_from_parametrization                                    *
+ *  Purpose:                                                                  *
+ *      Computes the vertices of a mesh from a parametric equation.           *
+ *  Arguments:                                                                *
+ *      canvas (Canvas *):                                                    *
+ *          The canvas for the animation. This contains geometry and buffers. *
+ *      f (SurfaceParametrization):                                           *
+ *          The function that defines the surface, z = f(x, y).               *
+ *  Output:                                                                   *
+ *      None (void).                                                          *
+ ******************************************************************************/
+void
+generate_mesh_from_parametrization(Canvas *canvas, SurfaceParametrization f)
+{
+    /*  Step sizes in the horizontal and vertical axes.                       */
+    const float dx = canvas->width / (float)(canvas->nx_pts - 1U);
+    const float dy = canvas->height / (float)(canvas->ny_pts - 1U);
+
+    /*  Variables for indexing the horizontal and vertical axes.              */
+    unsigned int x_index, y_index;
+
+    /*  Variable for indexing over the array being written to.                */
+    unsigned int index = 0;
+
+    /*  Avoid writing beyond the bounds of the array that was allocated.      *
+     *  Check if the input sizes are too big.                                 */
+    if ((canvas->nx_pts > MAX_WIDTH) || (canvas->ny_pts > MAX_HEIGHT))
+        return;
+
+    /*  Loop over the vertical axis. The surface is of the form z = f(x, y).  *
+     *  Note, since the y index is the outer for-loop, the array is indexed   *
+     *  in row-major fashion. That is, index = y * width + x.                 */
+    for (y_index = 0; y_index < canvas->ny_pts; ++y_index)
+    {
+        /*  Convert pixel index to y coordinate.                              */
+        const float y = canvas->vertical_start + (float)(y_index) * dy;
+
+        /*  Loop through the horizontal component of the object.              */
+        for (x_index = 0; x_index < canvas->nx_pts; ++x_index)
+        {
+            /*  Convert pixel index to x coordinate in the plane.             */
+            const float x = canvas->horizontal_start + (float)(x_index) * dx;
+
+            /*  Get the z component using the provided parametrization.       */
+            const float z = f(x, y);
+
+            /*  Add this point to our vertex array.                           */
+            canvas->mesh[index] = x;
+            canvas->mesh[index + 1U] = y;
+            canvas->mesh[index + 2U] = z;
+
+            /*  Move on to the next point in the mesh. A point needs 3 floats.*/
+            index += 3;
+        }
+        /*  End of horizontal for-loop.                                       */
+    }
+    /*  End of vertical for-loop.                                             */
+}
+/*  End of generate_mesh_from_parametrization.                                */
