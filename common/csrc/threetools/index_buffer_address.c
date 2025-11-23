7be0824cb72d8e03c4178893981c0a5f8e2c1e9f
ryanmaguire <rmaguire@protonmail.com>
2025-11-23 14:20:43 -0500

    Moving functions to common directory.

---
+++ common/csrc/threetools/index_buffer_address.c
@@ -0,0 +1,39 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of threejs_figures.                                     *
+ *                                                                            *
+ *  threejs_figures is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  threejs_figures is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with threejs_figures.  If not, see <https://www.gnu.org/licenses/>. *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Returns a pointer to the index buffer.                                *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       October 30, 2025                                              *
+ ******************************************************************************/
+
+/*  The index_buffer global is declared here.                                 */
+#include <threetools/globals.h>
+
+/*  Function prototype / forward declaration found here.                      */
+#include <threetools/threetools.h>
+
+/*  Function for getting the address of the index array.                      */
+unsigned int *index_buffer_address(void)
+{
+    /*  We can simply return the index buffer. At the JavaScript level this   *
+     *  is used to get the address of the index array for reading and writing.*/
+    return index_buffer;
+}
+/*  End of index_buffer_address.                                              */
