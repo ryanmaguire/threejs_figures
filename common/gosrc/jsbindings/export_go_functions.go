5bed7bd7ce930fab15d68317b79ddda57453261c
ryanmaguire <rmaguire@protonmail.com>
2025-11-23 10:18:45 -0500

    Update license.

--- common/gosrc/jsbindings/export_go_functions.go
+++ common/gosrc/jsbindings/export_go_functions.go
@@ -1,18 +1,20 @@
 /******************************************************************************
  *                                  LICENSE                                   *
  ******************************************************************************
- *  This file is free software: you can redistribute it and/or modify         *
+ *  This file is part of threejs_figures.                                     *
+ *                                                                            *
+ *  threejs_figures is free software: you can redistribute it and/or modify   *
  *  it under the terms of the GNU General Public License as published by      *
  *  the Free Software Foundation, either version 3 of the License, or         *
  *  (at your option) any later version.                                       *
  *                                                                            *
- *  This file is distributed in the hope that it will be useful,              *
+ *  threejs_figures is distributed in the hope that it will be useful,        *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
  *  GNU General Public License for more details.                              *
  *                                                                            *
  *  You should have received a copy of the GNU General Public License         *
- *  along with this file.  If not, see <https://www.gnu.org/licenses/>.       *
+ *  along with threejs_figures.  If not, see <https://www.gnu.org/licenses/>. *
  ******************************************************************************
  *  Purpose:                                                                  *
  *      Exports Go functions to JavaScript.                                   *
