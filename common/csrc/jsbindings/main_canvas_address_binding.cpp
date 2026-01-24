5fe0cfa0d125a338f1de9e7613b1c489ced80fef
ryanmaguire <rmaguire@protonmail.com>
2026-01-24 15:30:55 -0500

    Adding more bindings.

---
+++ common/csrc/jsbindings/main_canvas_address_binding.cpp
@@ -0,0 +1,33 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is free software: you can redistribute it and/or modify         *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  This file is distributed in the hope that it will be useful,              *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with this file.  If not, see <https://www.gnu.org/licenses/>.       *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Provides an emscripten binding for the main_canvas_address function.  *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       January 24, 2026                                              *
+ ******************************************************************************/
+#include <threetools/threetools.h>
+#include <emscripten/bind.h>
+
+EMSCRIPTEN_BINDINGS(threetools_main_canvas_address_function)
+{
+    emscripten::function(
+        "mainCanvasAddress",
+        &main_canvas_address,
+        emscripten::return_value_policy::take_ownership()
+    );
+}
