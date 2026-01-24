da57cf52ef1012802f27a044fdf60d62b9bb0491
ryanmaguire <rmaguire@protonmail.com>
2026-01-24 17:06:12 -0500

    Updating use of C++ binding functions.

--- common/jscommon/initGeometry.js
+++ common/jscommon/initGeometry.js
@@ -1,11 +1,17 @@
 import {BufferAttribute} from 'three';
-import {meshBufferAddress, indexBufferAddress, memory} from "wasmtools";
+import {
+    mainCanvasAddress,
+    indexBufferAddress,
+    meshBufferAddress,
+    memory
+} from "wasmtools";
 
 /*  Helper function for initializing the three.js geometry.                   */
 export function initGeometry(geometry, meshSize, indexSize) {
 
-    const meshPtr = meshBufferAddress();
-    const indexPtr = indexBufferAddress();
+    const canvasPtr = mainCanvasAddress();
+    const meshPtr = meshBufferAddress(canvasPtr);
+    const indexPtr = indexBufferAddress(canvasPtr);
 
     const meshBuffer = new Float32Array(memory.buffer, meshPtr, meshSize);
     const indexBuffer = new Uint32Array(memory.buffer, indexPtr, indexSize);
