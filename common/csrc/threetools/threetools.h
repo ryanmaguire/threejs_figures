2ecbcc8a13d685955ef0d1daac1f7f595f640034
ryanmaguire <rmaguire@protonmail.com>
2026-03-25 12:47:35 -0400

    Updating function parameters.

--- common/csrc/threetools/threetools.h
+++ common/csrc/threetools/threetools.h
@@ -86,8 +86,8 @@ extern void generate_rectangular_wireframe(Canvas * const canvas);
  *  Purpose:                                                                  *
  *      Returns a pointer to the index array.                                 *
  *  Arguments:                                                                *
- *      canvas (const Canvas * const).                                        *
- *          The canvas containing the index buffer that we want.              *
+ *      object (const Object * const).                                        *
+ *          The object containing the index buffer that we want.              *
  *  Output:                                                                   *
  *      address (unsigned int *):                                             *
  *          A pointer to the index array.                                     *
@@ -95,7 +95,7 @@ extern void generate_rectangular_wireframe(Canvas * const canvas);
  *      This function is called at the JavaScript level to get the address    *
  *      for the index array so it may read and write to it.                   *
  ******************************************************************************/
-extern unsigned int *index_buffer_address(const Canvas * const);
+extern unsigned int *index_buffer_address(const Object * const object);
 
 /******************************************************************************
  *  Function:                                                                 *
